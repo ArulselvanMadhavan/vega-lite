49cd5211405c5bcab26585987d9c19a4b4d634ee
Arulselvan Madhavan <arulselvan1234@gmail.com>
2022-09-18 13:55:49 -0700

    save all

--- scripts/gen_stats.py
+++ scripts/gen_stats.py
@@ -2,11 +2,12 @@ from gen_tree import chunks_from_name, group_per_depth, nodes_dict, names
 import torch
 import csv
 from save_load_meta_data import load_meta_data
+from scipy import stats
 
 data_dir = 'data/meta_data/'  # Data directory
 full_meta_data = load_meta_data(data_dir)
-metrics = ["bias", "weights", "inputs"]
-sample_len = 100
+metrics = ["bias", "weights"]
+sample_len = 200
 with open("stats.csv", "w+") as f:
     writer = csv.writer(f)
     writer.writerow(["id", "metric", "layer_name", "layer_member", "value", "density"])
@@ -16,22 +17,29 @@ with open("stats.csv", "w+") as f:
         words_per_depth = group_per_depth(words)
         for m in metrics:
             m_data = data[m]
-            m_data = torch.flatten(m_data).tolist()[:sample_len]
+            m_data = torch.flatten(m_data)
+            m_data_list = m_data.tolist()
+            kernel = stats.gaussian_kde(m_data_list)
+            samples = kernel.resample(size=sample_len, seed=31)
+            density = kernel.pdf(samples)
+            samples = samples.reshape(-1)
+            # print(samples.shape, density.shape, min(density), max(density))
+            # print(kernel.n)
             layer_id = nodes_dict[".".join(words_per_depth)]["id"]
             if len(words_per_depth) == 3:
                 rows = list(zip([layer_id] * sample_len,
                                 [m] * sample_len,
                                 [words_per_depth[1]] * sample_len,
                                 [words_per_depth[2]] * sample_len,
-                                m_data,
-                                [1]*sample_len))
+                                samples,
+                                density))
             else:
                 rows = list(zip([layer_id] * sample_len,
                                 [m] * sample_len,
                                 [words_per_depth[0]] * sample_len,
                                 [words_per_depth[0]] * sample_len,
-                                m_data,
-                                [1]*sample_len))
+                                samples,
+                                density))
             writer.writerows(rows)
         
         
